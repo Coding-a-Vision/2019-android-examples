07c1cd75c4f59ba86e2344343b1dd1f7e8096b41
Sasa Sekulic <ja@sasasekulic.info>
2019-11-06 23:02:43 +0100

    added bottom navigation example

---
+++ bottom-nav-app/src/main/java/examples/android2019/bottomnavtest/ui/home/HomeFragment.kt
@@ -0,0 +1,29 @@
+package examples.android2019.bottomnavtest.ui.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import com.migreat.bottomnavtest.R
+
+class HomeFragment : Fragment() {
+
+    private lateinit var homeViewModel: HomeViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        homeViewModel = HomeViewModel()
+        val root = inflater.inflate(R.layout.fragment_home, container, false)
+        val textView: TextView = root.findViewById(R.id.text_home)
+        homeViewModel.text.observe(this, Observer {
+            textView.text = it
+        })
+        return root
+    }
+}
