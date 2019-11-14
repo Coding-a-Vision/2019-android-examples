0ce142de736bdfd02b64c02bd9e600170ab1ef65
Sasa Sekulic <ja@sasasekulic.info>
2019-11-15 00:35:21 +0100

    added JSON printing to sharedprefs example

--- sharedprefs-app/src/main/java/examples/android2019/sharedprefs/SecondActivity.kt
+++ sharedprefs-app/src/main/java/examples/android2019/sharedprefs/SecondActivity.kt
@@ -5,6 +5,7 @@ import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
@@ -50,27 +51,43 @@ class SecondActivity : AppCompatActivity() {
             .create()
 
     private fun setupViews() {
-        val showDataView: TextView = findViewById(R.id.text_main)
+        val showDataView: TextView = findViewById(R.id.text_object)
+        val showJsonView: TextView = findViewById(R.id.text_json)
         val saveDataAction: Button = findViewById(R.id.action_save_data)
         val deleteDataAction: Button = findViewById(R.id.action_delete_data)
 
-        refreshDataActions(sharedPrefs, showDataView, saveDataAction, deleteDataAction)
+        refreshDataActions(
+            sharedPrefs,
+            showDataView,
+            showJsonView,
+            saveDataAction,
+            deleteDataAction
+        )
 
         saveDataAction.setOnClickListener {
             val newCustomObject = CustomObject("some text", Random.nextInt())
             val stringified = gson.toJson(newCustomObject)
+            Log.d("TEST", "SAVED stringified: $stringified, custom object: $newCustomObject")
+
             sharedPrefs.edit().putString(SHARED_PREFS_SECOND_DATA_TAG, stringified).apply()
-            refreshDataActions(sharedPrefs, showDataView, saveDataAction, deleteDataAction)
+            refreshDataActions(
+                sharedPrefs, showDataView,
+                showJsonView, saveDataAction, deleteDataAction
+            )
         }
         deleteDataAction.setOnClickListener {
             sharedPrefs.edit().remove(SHARED_PREFS_SECOND_DATA_TAG).apply()
-            refreshDataActions(sharedPrefs, showDataView, saveDataAction, deleteDataAction)
+            refreshDataActions(
+                sharedPrefs, showDataView,
+                showJsonView, saveDataAction, deleteDataAction
+            )
         }
     }
 
     private fun refreshDataActions(
         prefs: SharedPreferences,
         dataView: TextView,
+        jsonView: TextView,
         saveDataAction: Button,
         deleteDataAction: Button
     ) {
@@ -78,11 +95,17 @@ class SecondActivity : AppCompatActivity() {
             val stringified = prefs.getString(SHARED_PREFS_SECOND_DATA_TAG, "")
             val customObject = gson.fromJson(stringified, CustomObject::class.java)
 
-            dataView.text = "text: ${customObject.text}, number: ${customObject.number}, list: ${customObject.list}"
+            Log.d("TEST", "READ stringified: $stringified, custom object: $customObject")
+
+            jsonView.text = "JSON = text: $stringified"
+            jsonView.visibility = View.VISIBLE
+            dataView.text =
+                "Custom Object = text: ${customObject.text}, number: ${customObject.number}, list: ${customObject.list}"
             dataView.visibility = View.VISIBLE
             deleteDataAction.visibility = View.VISIBLE
             saveDataAction.visibility = View.GONE
         } else {
+            jsonView.visibility = View.GONE
             dataView.visibility = View.GONE
             deleteDataAction.visibility = View.GONE
             saveDataAction.visibility = View.VISIBLE
