a6021112714a381c7839c713f2e39d9b5dd2a520
Sasa Sekulic <ja@sasasekulic.info>
2019-11-16 01:31:09 +0100

    removed TODO from DynamicCategoryAdapter in RV app

--- recyclerview-app/src/main/java/examples/android2019/recyclerview/DynamicCategoryAdapter.kt
+++ recyclerview-app/src/main/java/examples/android2019/recyclerview/DynamicCategoryAdapter.kt
@@ -45,6 +45,6 @@ class CategoryDiffCallback : DiffUtil.ItemCallback<Category>() {
     }
 
     override fun areContentsTheSame(oldItem: Category, newItem: Category): Boolean {
-        return oldItem == newItem //TODO check this
+        return oldItem == newItem
     }
 }
