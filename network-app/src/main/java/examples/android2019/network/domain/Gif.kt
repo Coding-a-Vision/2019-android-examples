e3d6557c4d068bfa418464d13de513b3d3bc4192
Sasa Sekulic <ja@sasasekulic.info>
2019-12-02 10:51:21 +0100

    removed unfinished network-app example

--- network-app/src/main/java/examples/android2019/network/domain/Gif.kt
+++
@@ -1,3 +0,0 @@
-package examples.android2019.network.domain
-
-data class Gif(val name: String)
--- network-app/src/main/java/examples/android2019/network/util/ExtensionMethods.kt
+++
@@ -1,6 +0,0 @@
-package examples.android2019.network.util
-
-//https://www.reddit.com/r/Kotlin/comments/bzsiw3/force_exhaustive_whenstatement_sealed_classes/
-
-val <T> T.exhaustive: T
-    get() = this
